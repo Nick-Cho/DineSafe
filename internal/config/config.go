ff0dc26536ac163673e5307ba47fb1e42f9c3e2e
Nick Cho <nicholas.cho@hotmail.ca>
2023-05-27 09:38:00 -0400

    Setting up package that can be reused to connect to database

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+var (
+	ctx context.Context
+	db  *sql.DB
+)
+
+func Connect() *sql.DB {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "$Bigley2209"
+	dbName := "allergy_db"
+	dbIp := "tcp(localhost:3306)"
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	db, _ := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
+	if err := db.PingContext(ctx); err != nil {
+		panic(err.Error())
+	}
+	return db
+}
