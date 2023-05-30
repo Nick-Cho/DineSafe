c24aed8734d493387b15e45a0762200b4394bd6b
Nick Cho <nicholas.cho@hotmail.ca>
2023-05-29 23:09:59 -0400

    Creating API endpoint for register user

--- cmd/routes/register/handler.go
+++ cmd/routes/register/handler.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Nick-Cho/allergy-project/internal/config"
 	userModel "github.com/Nick-Cho/allergy-project/internal/models"
+	"github.com/Nick-Cho/allergy-project/internal/responses"
 	"github.com/aws/aws-lambda-go/events"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -14,21 +15,42 @@ import (
 type Handler struct{}
 
 func (h *Handler) HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// fmt.Println("Hello World")
 	var requestBody map[string]string
 	var user userModel.User
-	fmt.Println(user)
+
+	fmt.Println(user) //temp
+
 	db := config.Connect()
 	defer db.Close()
 
 	err := json.Unmarshal([]byte(request.Body), &requestBody)
 	if err != nil {
 		log.Println("error unmarshalling response body from register user request | ", err)
-		// return responses.ServerError(err), fmt.Errorf("error unmarshalling response body from spotify update user request")
+		return responses.ServerError(err), fmt.Errorf("error unmarshalling response body from create user request")
 	}
 
+	//Should check if the user exists first
+	email := requestBody["email"]
+	name := requestBody["name"]
+	password := requestBody["password"]
+
+	//Encrypt password before saving it in DB
+
+	_, err = db.Exec("INSERT INTO user(name, email, password) VALUES(?, ?, ?)", name, email, password)
+	fmt.Println(email, name, password) //temp
+
+	if err != nil {
+		log.Println("error creating new user", err)
+		return responses.ServerError(err), fmt.Errorf("error inserting new entry into user table")
+	}
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 202,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "*",
+			"Access-Control-Allow-Credentials": "true",
+		},
+		Body: "success",
 	}
 
 	return response, nil
