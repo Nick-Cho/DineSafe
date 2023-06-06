c00071239fcd00da64313d70276f32bc8b533313
Nick Cho <nicholas.cho@hotmail.ca>
2023-06-05 20:10:21 -0400

    updating config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,12 +3,13 @@ package config
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"log"
 	"time"
 )
 
 var (
-	ctx context.Context
-	db  *sql.DB
+	db *sql.DB
 )
 
 func Connect() *sql.DB {
@@ -16,12 +17,24 @@ func Connect() *sql.DB {
 	dbUser := "root"
 	dbPass := "$Bigley2209"
 	dbName := "allergy_db"
-	dbIp := "tcp(localhost:3306)"
+	dbIp := "tcp(127.0.0.1:3306)"
 
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	fmt.Println(db)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	res, err := db.ExecContext(ctx, "create database")
+	fmt.Println(res)
+	if err != nil {
+		log.Printf("Error %s when creating db\n", err)
+	}
+
 	db, _ := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(20)
+	db.SetConnMaxLifetime(time.Minute * 5)
+
 	if err := db.PingContext(ctx); err != nil {
 		panic(err.Error())
 	}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,7 @@
 package userModel
 
 type User struct {
-	Id       uint   `form:"id" json:"id"`
+	Id       int64  `form:"id" json:"id"`
 	Name     string `form:"name" json:"name"`
 	Email    string `form:"email" json:"email"`
 	Password string `form:"password" json:"password"`
