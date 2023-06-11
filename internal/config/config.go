968b4ee28a152c9f5febb3a5bf7dc23d25d5044d
Nick Cho <nicholas.cho@hotmail.ca>
2023-06-11 19:16:59 -0400

    Adding deferred connection cancel to the db configuration file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,7 @@
 package config
 
 import (
-	"context"
 	"database/sql"
-	"fmt"
 	"log"
 	"time"
 )
@@ -19,24 +17,16 @@ func Connect() *sql.DB {
 	dbName := "allergy_db"
 	dbIp := "tcp(127.0.0.1:3306)"
 
-	fmt.Println(db)
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
+	defer db.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	res, err := db.ExecContext(ctx, "create database")
-	fmt.Println(res)
 	if err != nil {
 		log.Printf("Error %s when creating db\n", err)
 	}
 
-	db, _ := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
 	db.SetMaxOpenConns(20)
 	db.SetMaxIdleConns(20)
 	db.SetConnMaxLifetime(time.Minute * 5)
 
-	if err := db.PingContext(ctx); err != nil {
-		panic(err.Error())
-	}
 	return db
 }
