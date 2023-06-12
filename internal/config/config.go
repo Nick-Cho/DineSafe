c387b34e657d91cb49f337f97d6fa10d815d6b18
Nick Cho <nicholas.cho@hotmail.ca>
2023-06-11 20:30:40 -0400

    Updating db config to have localhost instead of explicit ip

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,10 +15,9 @@ func Connect() *sql.DB {
 	dbUser := "root"
 	dbPass := "$Bigley2209"
 	dbName := "allergy_db"
-	dbIp := "tcp(127.0.0.1:3306)"
+	dbIp := "tcp(localhost:3306)"
 
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
-	defer db.Close()
 
 	if err != nil {
 		log.Printf("Error %s when creating db\n", err)
