1838215a3f4765c58462075435f313b9224725cf
Nick Cho <nicholas.cho@hotmail.ca>
2023-06-13 19:25:26 -0400

    Updating db config with GORM connection

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,31 +1,31 @@
 package config
 
 import (
-	"database/sql"
 	"log"
-	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 var (
-	db *sql.DB
+	db  *gorm.DB
+	err error
 )
 
-func Connect() *sql.DB {
+func Connect() *gorm.DB {
 	dbDriver := "mysql"
 	dbUser := "root"
 	dbPass := "$Bigley2209"
 	dbName := "allergy_db"
-	dbIp := "tcp(localhost:3306)"
+	dbIp := "tcp(allergy-project.cmdsxuexncin.us-east-1.rds.amazonaws.com:3306)"
 
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
+	db, err := gorm.Open(dbDriver, dbUser+":"+dbPass+"@"+dbIp+"/"+dbName)
 
 	if err != nil {
 		log.Printf("Error %s when creating db\n", err)
+	} else {
+		log.Println("Connection Established")
 	}
 
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(20)
-	db.SetConnMaxLifetime(time.Minute * 5)
-
+	defer db.Close()
 	return db
 }
