f658ec312a45a382f9885f3dc82e90ba7ff1d000
Nick Cho <nicholas.cho@hotmail.ca>
2023-08-24 20:16:42 -0400

    Fixing search restaurant API route to use query string parameters to build Google Maps Request

---
+++ cmd/routes/getRestaurantReviews/handler.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/Nick-Cho/allergy-project/internal/config"
+	"github.com/Nick-Cho/allergy-project/internal/responses"
+	"github.com/aws/aws-lambda-go/events"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Handler struct{}
+
+type ReviewInfo struct {
+	Id     int    `json:"id"`
+	Review string `json:"review"`
+}
+
+type RestaurantInfo struct {
+	StreetAddress string `json:"streetAddress"`
+	Name          string `json:"name"`
+	City          string `json:"city"`
+}
+
+func (h *Handler) HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var requestBody map[string]string
+	request.Body =
+		`{
+			"street_address": ""
+		}`
+	db := config.Connect()
+
+	err := json.Unmarshal([]byte(request.Body), &requestBody)
+
+	if err != nil {
+		log.Println("error unmarshalling response body from register user request | ", err)
+		return responses.ServerError(err), fmt.Errorf("error unmarshalling response body from create user request")
+	}
+
+	streetAddress := requestBody["street_address"]
+
+	//CHECK IF RESTAURANT EXISTS AND PUSH IN A NEW RESTAURANT INSTANCE IF IT DOESN'T AND RETURN BLANK ARRAY
+	// Seeing if the restaurant exists before trying to grab the reviews linked to it
+	restaurantCheck := fmt.Sprintf("SELECT * FROM allergy_db.Restaurants WHERE street_address='%s'", streetAddress)
+
+	res, err := db.Query(restaurantCheck)
+
+	var restaurant RestaurantInfo
+
+	for res.Next() {
+		// for each row, scan the result into our tag composite object
+		err = res.Scan(&restaurant.StreetAddress, &restaurant.Name, &restaurant.City)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// log.Printf("Queried restaurant name: %s", restaurant.Name)
+	}
+	if restaurant.Name == "" {
+		// Case where no restaurant currently exists with the address provided
+
+		// Call Search Restaurant endpoint and parse it to get the info needed for a new restaurant entry
+
+		// Call insertRestaurant with the info receieved to register the restaurant and then return null response body
+	}
+
+	sqlRequest := fmt.Sprintf(
+		`SELECT r.*
+			FROM allergy_db.Restaurants R
+			INNER JOIN restaurant_reviews rR
+			ON rR.restaurant_address = '%s'
+			INNER JOIN allergy_db.Reviews r
+			ON r.id = rR.review_id`,
+		streetAddress)
+
+	// fmt.Printf("sql GET request: %s\n", sqlRequest)
+	res, err = db.Query(sqlRequest)
+
+	// fmt.Printf("Response from db Query:  | %s ", res)
+	// Formatting MySQL response to JSON
+	var reviews []ReviewInfo
+	for res.Next() {
+		// for each row, scan the result into our tag composite object
+		var tempId int
+		var tempReview string
+
+		err = res.Scan(&tempId, &tempReview) // &tempId, &tempReview
+
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+		var reviewInst ReviewInfo
+		reviewInst.Id = tempId
+		reviewInst.Review = tempReview
+		reviews = append(reviews, reviewInst)
+		// log.Printf(review.Review)
+	}
+	// fmt.Printf("Response from db execution: %s\n", res)
+
+	if err != nil {
+		log.Println("Error pulling reviews", err)
+		return responses.ServerError(err), fmt.Errorf("error pulling review from inner join table: %s", err)
+	}
+	defer db.Close()
+
+	responseBody, err := json.Marshal(reviews)
+	if err != nil {
+		log.Println("ERROR MARSHALLING RESPONSE BODY TO JSON", err)
+		return responses.ServerError(err), fmt.Errorf("ERROR MARSHALLING RESPONSE BODY TO JSON")
+	}
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 202,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "*",
+			"Access-Control-Allow-Credentials": "true",
+		},
+		Body: string(responseBody),
+	}
+
+	return response, nil
+}
--- cmd/routes/searchRestaurant/handler.go
+++ cmd/routes/searchRestaurant/handler.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/Nick-Cho/allergy-project/internal/responses"
 	"github.com/aws/aws-lambda-go/events"
 )
 
 type Handler struct{}
 
 func (h *Handler) HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var requestBody map[string]string
-	request.Body =
-		`{
-			"search": "Yunshang",
-			"latitude": "43.8272",
-			"longitude": "-79.2788992"
-		}`
-	err := json.Unmarshal([]byte(request.Body), &requestBody)
+	// var requestBody map[string]string
+	// request.Body =
+	// 	`{
+	// 		"search": "Yunshang",
+	// 		"latitude": "43.8272",
+	// 		"longitude": "-79.2788992"
+	// 	}`
+	// err := json.Unmarshal([]byte(request.Body), &requestBody)
 
-	if err != nil {
-		log.Println("error unmarshalling lat and long | ", err)
-		return responses.ServerError(err), fmt.Errorf("error unmarshalling lat and long")
-	}
+	// if err != nil {
+	// 	log.Println("error unmarshalling lat and long | ", err)
+	// 	return responses.ServerError(err), fmt.Errorf("error unmarshalling lat and long")
+	// }
 
-	search := requestBody["search"]
-	latitude := requestBody["latitude"]
-	longitude := requestBody["longitude"]
-
-	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&locationbias=circle:20000@%s,%s&fields=formatted_address,name,rating,opening_hours,geometry&key=%s", search, latitude, longitude, os.Getenv("GOOGLE_API_KEY"))
+	search := request.QueryStringParameters["search"]
+	longitude := request.QueryStringParameters["longitude"]
+	latitude := request.QueryStringParameters["latitude"]
+	log.Println("search parameter: ", search)
+	log.Println("longitude parameter: ", longitude)
+	log.Println("latitude parameter: ", latitude)
+	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&locationbias=circle:20000@%s,%s&fields=formatted_address,name,rating,opening_hours&key=%s", search, latitude, longitude, os.Getenv("GOOGLE_API_KEY"))
 	method := "GET"
 
 	client := &http.Client{}
