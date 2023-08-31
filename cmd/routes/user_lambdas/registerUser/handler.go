25200b1588af43e2b0c5e8e832739f90923010e0
Nick Cho <nicholas.cho@hotmail.ca>
2023-08-31 13:54:30 -0400

    Adding encryption for user password when they register

--- cmd/routes/user_lambdas/registerUser/handler.go
+++ cmd/routes/user_lambdas/registerUser/handler.go
@@ -9,21 +9,19 @@ import (
 	"github.com/Nick-Cho/allergy-project/internal/responses"
 	"github.com/aws/aws-lambda-go/events"
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Handler struct{}
 
+const (
+	MinCost     int = 4  // the minimum allowable cost as passed in to GenerateFromPassword
+	MaxCost     int = 31 // the maximum allowable cost as passed in to GenerateFromPassword
+	DefaultCost int = 10 // the cost that will actually be set if a cost below MinCost is passed into GenerateFromPassword
+)
+
 func (h *Handler) HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var requestBody map[string]string
-
-	// request.Body =
-	// 	`{
-	// 		"name": "Nick",
-	// 		"email": "nicholas.cho@hotmail.ca",
-	// 		"password": "123"
-	// 	}`
-
-	// log.Println(request.Body)
 
 	db := config.Connect()
 
@@ -36,12 +34,20 @@ func (h *Handler) HandleRequest(request events.APIGatewayProxyRequest) (events.A
 	//Should check if the user exists first
 	email := requestBody["email"]
 	name := requestBody["name"]
-	password := requestBody["password"]
+	password := []byte(requestBody["password"])
 	fmt.Printf("Request email name and password: %s, %s, %s\n", email, name, password) //temp
 
 	//Encrypt password before saving it in DB
+	hashedPswd, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
 
-	sqlRequest := fmt.Sprintf("INSERT INTO allergy_db.Users (id, name, email, password) VALUES (2, '%s', '%s', '%s')", name, email, password)
+	if err != nil {
+		log.Println("error encrypting password | ", err)
+		return responses.ServerError(err), fmt.Errorf("error encrypting password")
+	}
+
+	convertedHashedPswd := string(hashedPswd)
+
+	sqlRequest := fmt.Sprintf("INSERT INTO allergy_db.Users (id, name, email, password) VALUES (2, '%s', '%s', '%s')", name, email, convertedHashedPswd)
 	fmt.Printf("sql POST request: %s\n", sqlRequest)
 	res, err := db.Exec(sqlRequest)
 
