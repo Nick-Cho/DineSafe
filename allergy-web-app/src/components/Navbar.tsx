a9e47db35ed3b6c9055e6ad0bf3c82db97e2404d
Nick Cho <nicholas.cho@hotmail.ca>
2023-09-01 17:34:28 -0400

    Transferring Progress

--- allergy-web-app/src/components/Navbar.tsx
+++ allergy-web-app/src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Navbar() {
   return (
     <nav>
-        <div className="bg-black text-white font-uber font-medium text-l space-x-4 flex">
-            <h1 className="text-2xl font-light py-4">
+        <div className="bg-black text-white font-uber font-medium text-l space-x-8 flex">
+            <h1 className="text-2xl font-light pl-3 py-4">
                 Placeholder
             </h1>
             <h1 className="py-5">
